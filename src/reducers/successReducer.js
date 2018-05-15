46a0953e001b16e926b999d0b49e7d39e70270a5
James McCormack <thejamesmccormack@gmail.com>
2018-05-15 15:56:11 -0700

    redux setup

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+/**
+* @function successReducer
+* @param {array} state - Array of guessed words.
+* @param {object} action - action to be reduced.
+* @returns {object} - new success state
+*/
+
+export default function (state, action) {
+  return null;
+}
