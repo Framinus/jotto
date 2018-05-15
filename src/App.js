15513d5040cf87c36ffd7c1009a031addc5bdebd
James McCormack <thejamesmccormack@gmail.com>
2018-05-15 15:42:29 -0700

    add minimal bootstrap styling to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import GuessedWords from './GuessedWords';
+import Congrats from './congrats';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={[
+          { guessedWord: 'train', letterMatchCount: 3 }
+        ]} />
       </div>
     );
   }
