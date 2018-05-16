8bcf4db79b24fc80eb877215eabfdc8ec787807f
James McCormack <thejamesmccormack@gmail.com>
2018-05-16 15:20:39 -0700

    add redux-thunk and guessedWordsReducer and create first integration test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,17 @@
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD',
 };
 
 /**
-* @function correctGuess
-* @returns {object} - Action object with type 'CORRECT_GUESS'
+* Returns Thunk function that dispatches GUESS_WORD action and (conditionally) CORRECT_GUESS action.
+* @function guessWord
+* @param {string} guessedWord - Guessed word.
+* @returns {function} - Redux Thunk function.
 */
 
-export function correctGuess() {
-  return {
-    type: actionTypes.CORRECT_GUESS,
+export function guessWord(guessedWord) {
+  return function(dispatch, getState) {
+
   };
-}
+};
